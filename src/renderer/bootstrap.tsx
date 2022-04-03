966a3e4f44605b720cbdd2185af5d3fd836be9fb
jimmy <jimmy@collabee.co>
2022-04-03 12:45:30 +0900

    react 18버전용 코드로 변경

--- src/renderer/bootstrap.tsx
+++ src/renderer/bootstrap.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Screenplay from '../../public/assets/text/Screenplay';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import text from '../../public/assets/text/text';
 
-const screenplay = new Screenplay(text);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App screenplay={screenplay} />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const id = 'root';
+const rootElem = document.getElementById(id);
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+} else {
+  console.log(
+    `${rootElem} 가 존재하지 않습니다. (${id} 가 존재하지 않을 수도 있습니다)`,
+  );
+}
