b74098bc04007506af21f842e46f797f7bdcdb54
jimmy <jimmy@collabee.co>
2022-04-05 15:08:43 +0900

    게시판 리스트 불러오기 적용

--- src/renderer/components/pages/Board/Board.tsx
+++ src/renderer/components/pages/Board/Board.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getBoardList from '../../../api';
 
 function Board() {
-  return <div>board</div>;
+  const [boardList, setBoardList] = useState<any[]>([]);
+
+  useEffect(() => {
+    const reulst = getBoardList();
+    reulst
+      .then((result: any) => {
+        setBoardList(result);
+      })
+      .catch(() => {
+        setBoardList([
+          { title: '게시판 리스트를 불러오는 것에 실패 하였습니다.' },
+        ]);
+      });
+  }, []);
+  return (
+    <div>
+      {boardList.map((board) => (
+        <div key={board.title}>{board.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Board;
